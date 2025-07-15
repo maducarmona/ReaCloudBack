545bad763833f6618af6640b4727a9812fea2193
Maria Eduarda Nebel Carmona <97537810+maducarmona@users.noreply.github.com>
2025-07-14 22:04:36 -0300

    Railway test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "mysql",
   dialectOptions: {
     ssl: {
-      require: true,
+      rejectUnauthorized: false,
     },
   },
   logging: false,
